ce058df6fd983cedfe4b96d03e39dcbff25252b7
simon <simon@yeacreate.com>
2020-02-07 08:38:54 +0800

    add chinese support

--- tools/flashrootfs.sh
+++ tools/flashrootfs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-./upgrade_tool wl 0x00810000 ../output/images/rootfs.ext2;
-#./upgrade_tool wl 0x01010000 ../output/images/rootfs.ext2;
+#./upgrade_tool wl 0x00810000 ../output/images/rootfs.ext2;
+./upgrade_tool wl 0x01010000 ../output/images/rootfs.ext2;
 ./upgrade_tool rd;
